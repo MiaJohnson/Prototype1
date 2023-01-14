246dc3f942ca37f464a28e0f9c4234c11aedf30a
MiaJohnson <MiaJohnson@users.noreply.github.com>
2023-01-14 15:17:27 -0500

    Unit 1 Final
    All The Unit 1 tutorial steps.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-  public float speed = 5.0f;
+  private float speed = 20.0f;
+  private float turnSpeed = 45.0f;
+  private float horizontalInput;
+  private float verticalInput;
 
   // Start is called before the first frame update
   void Start()
@@ -14,9 +17,14 @@ public class PlayerController : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    horizontalInput = Input.GetAxis("Horizontal");
+    verticalInput = Input.GetAxis("Vertical");
+
     // Move the vehicle forward.
-    // Forward = 0, 0, 1.
     // Time.deltaTime means it multiplies it as: x * time, y * time, z * time.
-    transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+    // Rotates the car based on horizontal input from user.
+    transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
   }
 }
